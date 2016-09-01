3a228602c2490f7a7d46344823bedb1acba2a120
qi weiyu <qi@weiyu.me>
2016-09-01 22:33:06 +0800

    update

--- regex/main.php
+++ regex/main.php
@@ -1,15 +1,60 @@
 <?php
 require_once(__DIR__.'/reg.php');
 
-$testStr = array(
-	'abc',
-	'abde',
-	'a1b23',
+$testData = array(
+	//test char
+	'a' => array(
+		'a'=>true,
+		'b'=>false,
+	),
+	//test digit
+	'4' => array(
+		'4'=>true,
+		'9'=>false,
+	),
+	//test cat
+	'abc123'=>array(
+		'abc123'=>true,
+		'ab23'=>false,
+		'abc1234'=>false,
+		'abc122'=>false,
+	),
+	//test or
+	'a1b2|x3y4'=>array(
+		'a1b2'=>true,
+		'x3y4'=>true,
+		'a1b2x3y4'=>false,
+		'a1x3y4'=>false,
+		'a1b2x3'=>false,
+	),
+	//test star
+	'ab*c'=>array(
+		'ac'=>true,
+		'abc'=>true,
+		'abbc'=>true,
+		'ab'=>false,
+		'bc'=>false,
+	),
+	//test brackets
+	'a(b|c)*dde'=>array(
+		'abcdde'=>true,
+		'adde'=>true,
+		'abcbcbdde'=>true,
+		'abcbcbcbce'=>false,
+	),
+	//test repeat
+	'a(a|b)*aabba'=>array(
+		'aabba'=>false,
+		'aaabba'=>true,
+		'aabbaaabba'=>true,
+		'aabbaa'=>false,
+	),
 );
 
-$testReg = array(
-		'[a-zA-Z]*',
-		'[a-z]'
-);
-
-$reg = new Reg('abc|123|abc(zsd)213|a*b*c*|123a*(c|d)*345');
+foreach($testData as $regStr=>$testList) {
+	$reg = new Reg($regStr);
+	foreach($testList as $test=>$res) {
+		$testRes = $reg->test($test);
+		printf("%-20s%-15s%-10s%-10s\n", $regStr, $test, $testRes?'MATCH':'MISMATCH', ($res == $testRes)?'PASS':'FAIL');
+	}
+}
--- regex/reg.php
+++ regex/reg.php
@@ -1,9 +1,7 @@
 <?php
-
 //1.解析正则表达式
 //2.生成跳转表
 //3.确认是否匹配
-
 
 /*
  * 字母
@@ -28,10 +26,16 @@
 class Reg {
 	private $_str = '';
 	private $_readPos = 0;
+	private $_nfa;
 	
 	public function __construct($str) {
-		$this->_str = $str;
+		$this->_str = strval($str);
+		$this->_nfa = new NFA();
 		$this->orExpr();
+	}
+
+	public function test($str) {
+		return $this->_nfa->run($str);
 	}
 
 	public function go() {
@@ -62,86 +66,121 @@ class Reg {
 	public function alpha() {
 		$this->_log('Entry alpha');
 		$ch = $this->read();
-		if($this->_isAlpha($ch)) {
-			$this->_log('find '.$ch);
-			$this->go();
-			$this->_log('Leave alpha');
-			return true;
+		if(!$this->_isAlpha($ch)) {
+			throw new Exception("Syntax Error : Unexpected '{$ch}', alpha expected");
 		}
-		throw new Exception("Syntax Error : Unexpected '{$ch}', alpha expected");
+		$this->_log('find '.$ch);
+		$this->go();
+		$this->_log('Leave alpha');
+		$status = new NFAStatus();
+		$this->_nfa->addJump($status->start, $status->end, $ch);
+		$this->_nfa->setStartStatus($status->start);
+		$this->_nfa->setEndStatus($status->end);
+		$this->_log('Leave alpha');
+		return $status;
 	}
 
 	public function digit() {
 		$this->_log('Entry digit');
 		$ch = $this->read();
-		if($this->_isDigit($ch)) {
-			$this->_log('find '.$ch);
-			$this->go();
-			$this->_log('Leave digit');
-			return true;
+		if(!$this->_isDigit($ch)) {
+			throw new Exception("Syntax Error : Unexpected '{$ch}', digit expected");
 		}
-		throw new Exception("Syntax Error : Unexpected '{$ch}', digit expected");
+		$this->_log('find '.$ch);
+		$this->go();
+		$this->_log('Leave digit');
+		$status = new NFAStatus();
+		$this->_nfa->addJump($status->start, $status->end, $ch);
+		$this->_nfa->setStartStatus($status->start);
+		$this->_nfa->setEndStatus($status->end);
+		$this->_log('Leave digit');
+		return $status;
 	}
 
 	public function ch() {
 		$this->_log('Entry ch');
 		$ch = $this->read();
 		if($this->_isDigit($ch)) {
-			$this->digit();
+			$status = $this->digit();
 		}
 		else if($this->_isAlpha($ch)) {
-			$this->alpha();
+			$status = $this->alpha();
 		}
 		else {
 			throw new Exception("Syntax Error : Unexpected '{$ch}', alpha or digit expected");
 		}
+		$this->_nfa->setStartStatus($status->start);
+		$this->_nfa->setEndStatus($status->end);
 		$this->_log('Leave ch');
+		return $status;
 	}
 
 	public function expr() {
 		$this->_log('Entry expr');
 		if($this->read() == '(') {
-			$this->match('(');$this->orExpr();$this->match(')');
+			$this->match('(');$status = $this->orExpr();$this->match(')');
 		}
 		else {
-			$this->ch();
+			$status = $this->ch();
 		}
+		$this->_nfa->setStartStatus($status->start);
+		$this->_nfa->setEndStatus($status->end);
 		$this->_log('Leave expr');
+		return $status;
 	}
 
 	public function star() {
 		$this->_log('Entry star');
-		$this->expr();
+		$status = $this->expr();
 		if($this->read() == '*') {
 			$this->match('*');
+			$this->_nfa->addJump($status->start, $status->end, NFA::EMP);
+			$this->_nfa->addJump($status->end, $status->start, NFA::EMP);
 		}
+		$this->_nfa->setStartStatus($status->start);
+		$this->_nfa->setEndStatus($status->end);
 		$this->_log('Leave star');
+		return $status;
 	}
 
 	public function cat() {
 		$this->_log('Entry cat');
-		$this->star();
+		$status = $this->star();
 		while(true) {
 			if(in_array($this->read(), array('|',')', null), true)) break;
-			$this->star();
+			$starStatus = $this->star();
+			$this->_nfa->addJump($status->end, $starStatus->start, NFA::EMP);
+			$status->end = $starStatus->end;
 		}
+		$this->_nfa->setStartStatus($status->start);
+		$this->_nfa->setEndStatus($status->end);
 		$this->_log('Leave cat');
+		return $status;
 	}
 
 	public function orExpr() {
 		$this->_log('Entry or');
-		$this->cat();
+		$status = $this->cat();
 		while(true) {
 			if($this->read() == '|'){
-				$this->match('|');$this->cat();
+				$this->match('|');
+				$catStatus = $this->cat();
+				$orStatus = new NFAStatus();
+				$this->_nfa->addJump($orStatus->start, $status->start, NFA::EMP);
+				$this->_nfa->addJump($orStatus->start, $catStatus->start, NFA::EMP);
+				$this->_nfa->addJump($status->end, $orStatus->end, NFA::EMP);
+				$this->_nfa->addJump($catStatus->end, $orStatus->end, NFA::EMP);
+				$status = $orStatus;
 			}
 			else {
 				break;
 			}
 		}
+		$this->_nfa->setStartStatus($status->start);
+		$this->_nfa->setEndStatus($status->end);
 		$this->_log('Leave or');
+		return $status;
 	}
-
 
 	private function _isAlpha($ch) {
 		return ord($ch) >= ord('a') && ord($ch) <= ord('z');
@@ -152,6 +191,80 @@ class Reg {
 	}
 
 	private function _log($str) {
-		var_dump($str);
+		//var_dump($str);
+	}
+}
+
+class NFAStatus {
+	public $start;
+	public $end;
+	public function __construct() {
+		$this->start = self::getNextStatusId();
+		$this->end = self::getNextStatusId();
+	}
+
+	public static function getNextStatusId() {
+		static $id = 1;
+		return $id++;
+	}
+}
+
+class NFA {
+	const EMP = -1;
+	protected $_jumpTable = array();
+	protected $_startStatus = null;
+	protected $_endStatus = null;
+	protected $_statusList = array();
+	public function addJump($start, $end, $char) {
+		if(!isset($this->_jumpTable[$start])) {
+			$this->_jumpTable[$start] = array();
+		}
+		if(!isset($this->_jumpTable[$start][$char])) {
+			$this->_jumpTable[$start][$char] = array();
+		}
+		$this->_jumpTable[$start][$char][$end] = $end;
+	}
+	public function setStartStatus($status) {
+		$this->_startStatus = $status;
+	}
+	public function setEndStatus($status) {
+		$this->_endStatus = $status;
+	}
+	public function run($str) {
+		//计算start status的empClosure
+		//对每个char，计算move和它的empClosure
+		//char结束后，看是否在endStatus里面
+		$str = strval($str);
+		$this->_statusList = array();
+		$this->_addStatus($this->_startStatus);
+		$len = strlen($str);
+		for($i = 0; $i < $len; $i++) {
+			$ch = $str[$i];
+			$oldStatusList = $this->_statusList;
+			$this->_statusList = array();
+			foreach($oldStatusList as $status) {
+				$jumpStatusList = $this->_move($status, $ch);
+				foreach($jumpStatusList as $jumpStatus) {
+					$this->_addStatus($jumpStatus);
+				}
+			}
+		}
+		return isset($this->_statusList[$this->_endStatus]);
+	}
+	protected function _addStatus($status) {
+		if(!isset($this->_statusList[$status])) {
+			$this->_statusList[$status] = $status;
+			foreach($this->_move($status, self::EMP) as $status) {
+				if(!isset($this->_statusList[$status])) {
+					$this->_addStatus($status);
+				}
+			}
+		}
+	}
+	protected function _move($status, $ch) {
+		if(isset($this->_jumpTable[$status][$ch])) {
+			return $this->_jumpTable[$status][$ch];
+		}
+		return array();
 	}
 }
